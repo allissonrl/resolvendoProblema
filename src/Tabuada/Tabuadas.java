c2878fb7bd65dc814a25ff35068bdec86fa38ce3
allissonrl <rose.allisson@gmail.com>
2022-01-20 22:43:36 -0300

    criacao de tabuada

---
+++ src/Tabuada/Tabuadas.java
@@ -0,0 +1,24 @@
+package Tabuada;
+
+import javax.security.sasl.SaslClient;
+import java.util.Scanner;
+
+public class Tabuadas {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+    System.out.println("tabuada");
+        int tabuada = scan.nextInt();
+
+        System.out.println("tabuada de: "+ tabuada);
+
+        for ( int i = 1; i <= 10; i++) {
+            System.out.println(tabuada + "x" + i + "=" + (tabuada *i));
+
+
+        }
+
+
+    }
+}
